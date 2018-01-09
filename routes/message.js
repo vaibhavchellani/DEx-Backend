7ffcd047a3bec81c79e2be8d66d8b33f19552680
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-01-09 17:37:04 +0530

    order book done

--- routes/message.js
+++ routes/message.js
@@ -12,8 +12,11 @@ var ethAvailableVolumeBase=0;
 var availableVolume=0;
 var ethAvailableVolume=0;
 var price=0;
-var direction='null';
+var direction='none';
 var amount=0;
+var config = require('../resources/config.js');
+var ABI=[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_amount","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"totalSupply","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}];
+var utility=require('../resources/utility');
 router.post('/',function (req,res,next) {
     /*  request.get('http://api.etherscan.io/api?module=contract&action=getabi&address='+req.body.contractAddr,function (err,data) {
         //var contractABI = "";
@@ -21,12 +24,59 @@ router.post('/',function (req,res,next) {
         //var ABI=JSON.parse(data.body).result;
         //console.log(ABI.toArray());
     });*/
+    var item= formOrder(req);
+    console.log('returned item to be pushed to DB is '+item);
+    //var result=getDivisor('0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0');
+    //console.log(result);
+    //console.log(utility.weiToEth(1000000,10));
 
+    var Order=require('../models/Order');
+        Order.create(item,function (err,post) {
+           if(err) throw err;
+           res.json(post);
 
+        });
+
+});
+
+function getDivisor(tokenOrAddress) {
+    /*if (ABI!=null){
+        var MyContract = web3.eth.contract(ABI);
+        if(req.body.tokenGet!='0x0000000000000000000000000000000000000000')
+            var myContractInstance = MyContract.at(req.body.tokenGet.toString());
+        else if(req.body.tokenGive!='0x0000000000000000000000000000000000000000')
+            var myContractInstance = MyContract.at(req.body.tokenGive.toString());
+        else
+            throw "invalid address pair";
+
+        decimal = myContractInstance.decimals();
+
+        console.log(parseInt(decimal.toPrecision()) + typeof parseInt(decimal.toPrecision()));
+
+    } else {
+        console.log("Error" );
+    }*/
+    console.log('inside get divisor');
+    let result = 1000000000000000000;
+    const token =getToken(tokenOrAddress);
+    if (token && token.decimals >= 0) {
+        result = Math.pow(10, token.decimals); // eslint-disable-line no-restricted-properties
+    }
+    return new BigNumber(result);
+}
+function formOrder(req){
+    console.log(req.body);
+    if(req.body.tokenGet=='0x0000000000000000000000000000000000000000' && req.body.tokenGive!='0x0000000000000000000000000000000000000000') {
+        direction = 'sell';
+    }
+    else if(req.body.tokenGet!='0x0000000000000000000000000000000000000000' && req.body.tokenGive=='0x0000000000000000000000000000000000000000'){
+        direction='buy';
+    }
+    console.log('direction determined to be '+direction);
     const id = sha256(Math.random().toString());
     const buyOrder = {
         amount: req.body.amountGet,
-        price: req.body.amountGive
+        price: new BigNumber(req.body.amountGive)
             .div(req.body.amountGet)
             .mul(getDivisor(req.body.tokenGet))
             .div(getDivisor(req.body.tokenGive)),
@@ -44,91 +94,15 @@ router.post('/',function (req,res,next) {
             r: req.body.r,
             user: req.body.user,
         },
-
-
     };
     const sellOrder = {
-        amount: -message.amountGive,
-        price: message.amountGet
-            .div(message.amountGive)
-            .mul(API.getDivisor(message.tokenGive))
-            .div(API.getDivisor(message.tokenGet)),
+        amount: -req.body.amountGive,
+        price: new BigNumber(req.body.amountGet)
+            .div(req.body.amountGive)
+            .mul(getDivisor(req.body.tokenGive))
+            .div(getDivisor(req.body.tokenGet)),
         id: `${id}_sell`,
-        order: message,
-        updated: undefined,
-    };
-
-    if (order.amount >= 0) {
-        order.price = new BigNumber(order.order.amountGive)
-            .div(new BigNumber(order.order.amountGet))
-            .mul(API.getDivisor(order.order.tokenGet))
-            .div(API.getDivisor(order.order.tokenGive));
-        order.availableVolume = availableVolume;
-        order.ethAvailableVolume = this.utility.weiToEth(
-            Math.abs(order.availableVolume),
-            API.getDivisor(order.order.tokenGet));
-        order.availableVolumeBase = Math.abs(availableVolume
-            .mul(order.price)
-            .mul(API.getDivisor(order.order.tokenGive))
-            .div(API.getDivisor(order.order.tokenGet)));
-        order.ethAvailableVolumeBase = this.utility.weiToEth(order.availableVolumeBase,
-            API.getDivisor(order.order.tokenGive));
-    } else {
-        order.price = new BigNumber(order.order.amountGet)
-            .div(new BigNumber(order.order.amountGive))
-            .mul(API.getDivisor(order.order.tokenGive))
-            .div(API.getDivisor(order.order.tokenGet));
-        order.availableVolume = availableVolume
-            .div(order.price)
-            .mul(API.getDivisor(order.order.tokenGive))
-            .div(API.getDivisor(order.order.tokenGet));
-        order.ethAvailableVolume = this.utility.weiToEth(
-            Math.abs(order.availableVolume),
-            API.getDivisor(order.order.tokenGive));
-        order.availableVolumeBase = Math.abs(availableVolume);
-        order.ethAvailableVolumeBase = this.utility.weiToEth(
-            order.availableVol umeBase,
-            API.getDivisor(order.order.tokenGet));
-    }
-
-
-    /*if(req.body.tokenGet=='0x0000000000000000000000000000000000000000' && req   .body.tokenGive!='0x0000000000000000000000000000000000000000')
-    {
-        direction='sell';
-        amount = -req.body.amountGive;
-        availableVolumeBase=parseInt(req.body.amountGet);
-        ethAvailableVolumeBase=availableVolumeBase*Math.pow(10,-parseInt(decimal.toPrecision()));
-        availableVolume=parseInt(req.body.amountGive);
-        ethAvailableVolume = availableVolume*Math.pow(10,-18);
-        price =ethAvailableVolumeBase/ethAvailableVolume;
-    }
-    else if(req.body.tokenGet!='0x0000000000000000000000000000000000000000' && req.body.tokenGive=='0x0000000000000000000000000000000000000000'){
-        direction='buy';
-        amount = req.body.amountGet;
-        availableVolumeBase=parseInt(req.body.amountGet);
-        ethAvailableVolumeBase=availableVolumeBase*Math.pow(10,-18);
-        availableVolume=parseInt(req.body.amountGive);
-        ethAvailableVolume=availableVolume*Math.pow(10,-parseInt(decimal.toPrecision()));
-        price = ethAvailableVolumeBase/ethAvailableVolume;
-    }
-    else
-    {
-        direction='null';
-    }
-
-    var s={
-        amount:amount,
-        price:price,
-        availableVolumeBase:availableVolumeBase,
-        ethAvailableVolumeBase:ethAvailableVolumeBase,
-        availableVolume:availableVolume,
-        ethAvailableVolume:ethAvailableVolume
-    };
-    console.log(s);*/
-    var item = {
-        amount: "12",
-        price: "12",
-        order: {
+        order:{
             contractAddr: req.body.contractAddr,
             tokenGet: req.body.tokenGet,
             amountGet: req.body.amountGet,
@@ -140,85 +114,67 @@ router.post('/',function (req,res,next) {
             s: req.body.s,
             r: req.body.r,
             user: req.body.user,
-        },
-        id: "sdsdsd_sell",
+        } ,
 
-        availableVolume: "1",
-        ethAvailableVolume: "4",
-        availableVolumeBase: "1212",
-        ethAvailableVolumeBase: "12",
-        amountFilled: "0",
     };
-
-
-    var Order=require('../models/Order');
-        Order.create(item,function (err,post) {
-           if(err) throw err;
-           res.json(post);
-
-        });
-
-});
-
-function getDivisor(tokenOrAddress) {
-    var ABI=[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_amount","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"totalSupply","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}];
-
-    if (ABI!=null){
-        var MyContract = web3.eth.contract(ABI);
-        if(req.body.tokenGet!='0x0000000000000000000000000000000000000000')
-            var myContractInstance = MyContract.at(req.body.tokenGet.toString());
-        else if(req.body.tokenGive!='0x0000000000000000000000000000000000000000')
-            var myContractInstance = MyContract.at(req.body.tokenGive.toString());
-        else
-            throw "invalid address pair";
-        decimal = myContractInstance.decimals();
-
-        console.log(parseInt(decimal.toPrecision()) + typeof parseInt(decimal.toPrecision()));
-
-    } else {
-        console.log("Error" );
+    var sendOrder={};
+    if(direction==='buy'){
+        console.log('buy order to be sent');
+        sendOrder=buyOrder;
+    } else if(direction==='sell'){
+        console.log('sell order to be sent');
+        sendOrder=sellOrder;
     }
 
+    console.log(sendOrder);
+    var newOrder=getOrderParams(sendOrder,100);
+    console.log(newOrder);
+    return newOrder;
 
-    var result = 1000000000000000000;
 
-    result = Math.pow(10, parseInt(decimal.toPrecision())); // eslint-disable-line no-restricted-properties
-
-    return new BigNumber(result);
-};
+}
 function getOrderParams(orderIn,availableVolume) {
     var order=orderIn;
+    availableVolume=new BigNumber(1000000000000000000);
+    console.log('inside getOrderParams with input order as'+order);
     if (order.amount >= 0) {
+        console.log('inside if of getOrderParams');
         order.price = new BigNumber(order.order.amountGive)
             .div(new BigNumber(order.order.amountGet))
             .mul(getDivisor(order.order.tokenGet))
             .div(getDivisor(order.order.tokenGive));
+        // difference
         order.availableVolume = availableVolume;
         order.ethAvailableVolume = weiToEth(
             Math.abs(order.availableVolume),
             getDivisor(order.order.tokenGet));
+        //difference
         order.availableVolumeBase = Math.abs(availableVolume
             .mul(order.price)
-            .mul(API.getDivisor(order.order.tokenGive))
-            .div(API.getDivisor(order.order.tokenGet)));
-        order.ethAvailableVolumeBase = this.utility.weiToEth(order.availableVolumeBase,
+            .mul(getDivisor(order.order.tokenGive))
+            .div(getDivisor(order.order.tokenGet)));
+        order.ethAvailableVolumeBase = utility.weiToEth(order.availableVolumeBase,
             getDivisor(order.order.tokenGive));
-    } else {
+    }
+    else {
+        console.log('inside else of getOrderParams');
         order.price = new BigNumber(order.order.amountGet)
             .div(new BigNumber(order.order.amountGive))
-            .mul(API.getDivisor(order.order.tokenGive))
-            .div(API.getDivisor(order.order.tokenGet));
+            .mul(getDivisor(order.order.tokenGive))
+            .div(getDivisor(order.order.tokenGet));
+        //difference
         order.availableVolume = availableVolume
             .div(order.price)
-            .mul(API.getDivisor(order.order.tokenGive))
-            .div(API.getDivisor(order.order.tokenGet));
-        order.ethAvailableVolume = this.utility.weiToEth(
+            .mul(getDivisor(order.order.tokenGive))
+            .div(getDivisor(order.order.tokenGet));
+        order.ethAvailableVolume = utility.weiToEth(
             Math.abs(order.availableVolume),
-            API.getDivisor(order.order.tokenGive));
+            getDivisor(order.order.tokenGive));
+        //difference
         order.availableVolumeBase = Math.abs(availableVolume);
-        order.ethAvailableVolumeBase = this.utility.weiToEth(
-            order.availableVol umeBase,
-            API.getDivisor(order.order.tokenGet));
+        order.ethAvailableVolumeBase = utility.weiToEth(
+            order.availableVolumeBase,
+            getDivisor(order.order.tokenGet));
     }
     return order;
 
@@ -230,6 +186,85 @@ function getAvailableVolume()
 function weiToEth(wei, divisorIn) {
     const divisor = !divisorIn ? 1000000000000000000 : divisorIn;
     return (wei / divisor).toFixed(3);
-};
+}
+function getToken(token_address){
+    console.log('inside get token');
+    let result;
+
+    const matchingTokens = config.tokens.filter(
+        x => x.addr === token_address);
+    const expectedKeys = JSON.stringify([
+        'addr',
+        'decimals',
+        'name',
+    ]);
+    //matchingTokens++;
+    console.log(config.tokens.filter(x=>x.addr==token_address));
+    //console.log('matching tokens'+matchingTokens);
+    //console.log('matching token found '+matchingTokens[0]+' '+matchingTokens.length+' '+matchingTokens);
+    if (matchingTokens.length > 0) {
+        result = matchingTokens[0];
+        console.log('inside if of getToken');
+    } else if (token_address.addr && JSON.stringify(Object.keys(token_address).sort()) === expectedKeys) {
+        result = token_address;
+    }
+    return result;
+}
+/*function getAvailableVolume()
+{
+    const data = contract[functionName].getData.apply(null, args);
+    let url =
+    `https://${
+        config.ethTestnet ? config.ethTestnet : 'api'
+        }.etherscan.io/api?module=proxy&action=eth_Call&to=${
+        address
+        }&data=${
+        data}`;
+    if (config.etherscanAPIKey) url += `&apikey=${config.etherscanAPIKey}`;
+    utility.getURL(url, (err, body) => {
+        if (!err) {
+            try {
+                const result = JSON.parse(body);
+                const functionAbi = contract.abi.find(element => element.name === functionName);
+                const solidityFunction = new SolidityFunction(web3.Eth, functionAbi, address);
+                const resultUnpacked = solidityFunction.unpackOutput(result.result);
+                callback(undefined, resultUnpacked);
+            } catch (errJson) {
+                if (retries > 0) {
+                    setTimeout(() => {
+                        proxy(retries - 1);
+                    }, 1000);
+                } else {
+                    callback(err, undefined);
+                }
+            }
+        } else {
+            callback(err, undefined);
+        }
+    });
+}
+try {
+    if (web3In.currentProvider) {
+        const data = contract[functionName].getData.apply(null, args);
+        web3In.eth.call({ to: address, data }, (err, result) => {
+            if (!err) {
+                const functionAbi = contract.abi.find(element => element.name === functionName);
+                const solidityFunction = new SolidityFunction(web3In.Eth, functionAbi, address);
+                try {
+                    const resultUnpacked = solidityFunction.unpackOutput(result);
+                    callback(undefined, resultUnpacked);
+                } catch (errJson) {
+                    proxy(1);
+                }
+            } else {
+                proxy(1);
+            }
+        });
+    } else {
+        proxy(1);
+    }
+} catch (err) {
+    proxy(1);
+}*/
 
 module.exports=router;
